13679dbfe5e4c3b819fc54687fdb0af91da72f6d
pylypchukroman <romanpylypchuk@gmail.com>
2025-12-22 18:16:35 +1100

    added test api endpoint for balance

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import cors from "cors";
+import { pokerRooms } from './data/rooms.js';
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express + TypeScript!");
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.get("/balance", (req, res) => {
+  res.status(200).json(pokerRooms);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
