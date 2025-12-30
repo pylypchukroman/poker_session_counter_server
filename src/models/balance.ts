903a52b8634743801fd25de854cc984dc4eba375
pylypchukroman <romanpylypchuk@gmail.com>
2025-12-30 20:36:37 +1100

    remover _id and version from response

--- src/models/balance.ts
+++ src/models/balance.ts
@@ -1,17 +1,35 @@
 import { model, Schema } from 'mongoose';
 import { handleMongooseError } from '../helpers/handleMongooseError.ts';
 
-const balanceSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for balance'],
+const balanceSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for balance'],
+    },
+    balance: {
+      type: Number,
+      required: true,
+    },
   },
-  balance: {
-    type: Number,
-    required: true
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+    toObject: {
+      transform(doc, ret) {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
-balanceSchema.post("save", handleMongooseError);
+balanceSchema.post('save', handleMongooseError);
 
-export const Balance = model("balance", balanceSchema);
+export const Balance = model('balance', balanceSchema);
